00d98e6a519c21bafdccb04c820273b1cba798bc
biokpl <biokpl@github.com>
2026-03-27 22:36:54 +0300

    fix: SW cache v3 (önbellek temizle), not rengi kısıldı

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gridtracker-v2';
+const CACHE = 'gridtracker-v3';
 const ASSETS = ['./bist_tracker.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
