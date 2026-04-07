944b8db8bcf2c78aada2cd0a3a9267a177d81795
biokpl <biokpl@github.com>
2026-04-07 21:39:58 +0300

    fix: Task Scheduler ile baslatma, bat+vbs kaldirildi (Kaspersky uyumu)

--- setup_startup.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
-copy "%~dp0start_server.vbs" "%STARTUP%\GridTracker_Server.vbs" > nul
-wscript "%STARTUP%\GridTracker_Server.vbs"
--- start_server.vbs
+++
@@ -1,2 +0,0 @@
-Set oShell = CreateObject("WScript.Shell")
-oShell.Run "pythonw """ & Replace(WScript.ScriptFullName, "start_server.vbs", "server.py") & """", 0, False
