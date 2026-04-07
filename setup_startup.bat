5e369e4286b37408594935cecddf13a499f6413d
biokpl <biokpl@github.com>
2026-04-07 21:34:57 +0300

    fix: setup_startup.bat sessiz calistir, terminal acma

--- setup_startup.bat
+++ setup_startup.bat
@@ -1,6 +1,4 @@
 @echo off
 set STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 copy "%~dp0start_server.vbs" "%STARTUP%\GridTracker_Server.vbs" > nul
-echo GridTracker sunucusu Windows baslangicinа eklendi.
-echo Bir sonraki oturumdan itibaren otomatik baslar.
-pause
+wscript "%STARTUP%\GridTracker_Server.vbs"
